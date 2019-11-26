f3fa4fe58a068ce4dcee3f749401441f2dafd2da
ac-arpan <arpanchowdhury050@gmail.com>
2019-11-26 18:30:04 +0530

    upload folder added

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import *
 import pdfcropper
+from werkzeug import secure_filename
+import os
 
 app = Flask(__name__)
 
 
+app.config['UPLOAD_FOLDER'] = "C:\\Users\\DELL\\Desktop\\WEB_DEV\\Pdf Cropper"
 @app.route('/')
 def upload():
         
@@ -18,6 +21,8 @@ def success():
     start = int(request.form['start'])
     end = int(request.form['end'])
     f = request.files['file']
+    
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
         
     global file 
     file = f.filename
@@ -27,13 +32,14 @@ def success():
 
 @app.route('/convert',methods=["GET","POST"])
 def cropper():
+    
     pdfcropper.cropper(start,end,file)
     
     return render_template('download.html')
 
 @app.route('/download')
 def download():
-    filename = file.split(".")[0] + "cropped.pdf"
+    filename = file.split(".")[0] + " cropped.pdf"
     return send_file(filename,as_attachment = True)
     
     
